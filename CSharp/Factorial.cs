9402c2f7873236339fdcc217f15db5d8de4e02ed
prabhjyot009 <prabh156dps@gmail.com>
2023-09-08 21:25:20 +0530

    factorial

--- CSharp/Factorial.cs
+++ CSharp/Factorial.cs
@@ -8,10 +8,27 @@ namespace CSharp
 {
     class Factorial
     {
-        static int fact = 0;
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter number");
+            int n = Int32.Parse(Console.ReadLine());
+            int fact = 1;
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    fact = fact * i;
+            //}
+            if (n<0)
+            {
+                Console.WriteLine("Please provide value greater than zero");
+            }
+            else
+            {
+                for (int i = n; i >= 1; i--)
+                {
+                    fact *= fact;
+                }
+                Console.WriteLine("{0}! is {1}", n, fact);
+            }
         }
     }
 }
