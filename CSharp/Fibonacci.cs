b85a43aaf2da0c2e5768b865b186176f892f30d6
prabhjyot009 <prabh156dps@gmail.com>
2023-09-08 21:17:08 +0530

    Basic programs

---
+++ CSharp/Fibonacci.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class Fibonacci
+    {
+        static int p1 = 0, p2 = 1, p3;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number:");
+            int count = Int32.Parse(Console.ReadLine());
+            Console.Write(p1+" "+p2);
+            for (int i = 2; i < count; i++)
+            {
+                p3 = p1 + p2;
+                Console.Write(" "+p3);
+                p1 = p2;
+                p2 = p3;
+            }
+            Console.WriteLine();
+        }
+    }
+}
