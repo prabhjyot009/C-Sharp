713abcad81657c348b9e6dc604ee64860648d334
prabhjyot009 <prabh156dps@gmail.com>
2023-09-11 19:19:46 +0530

    Prime number in string

---
+++ CSharp/StringPrime.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class StringPrime
+    {
+        static void Main(string[] args)
+        {
+            string str = "12345Hello";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (isPrime(str[i]))
+                {
+                    System.Console.WriteLine(str[i]);
+                }
+            }
+        }
+        static bool isPrime(char ch)
+        {
+            int n = ch - '0';
+            if (n <= 1)
+            {
+                return false;
+            }
+            for (int i = 2; i * i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
