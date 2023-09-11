8ec12d8f09a586422cd466da7603f6f594a7a9e0
prabhjyot009 <prabh156dps@gmail.com>
2023-09-11 18:37:29 +0530

    commit

---
+++ CSharp/PrimeInArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class PrimeInArray
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5,7 };
+            int j;
+            foreach (int i in arr)
+            {
+                for ( j = 2; j < arr[i]; j++)
+                {
+                    if (arr[i]%j==0)
+                    {
+                        Console.WriteLine(i + "is not prime");
+                        Console.ReadLine();
+                        break;
+                    }
+                    if (j == arr[i])
+                    {
+                        Console.WriteLine(i + " is prime");
+                        Console.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
