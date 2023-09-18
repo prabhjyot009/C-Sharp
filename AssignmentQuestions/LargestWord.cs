678ac6ed197d248a160e322af00dccc0532067fc
prabhjyot009 <prabh156dps@gmail.com>
2023-09-19 01:26:36 +0530

    Largest Word in string

---
+++ AssignmentQuestions/LargestWord.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AssignmentQuestions
+{
+    class Largestword
+    {
+        static void Main(string[] args)
+        {
+            string s = "Google Doc Sirrrr";
+            string[] a = s.Split(' ');
+            string largestword = "";
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i].Length > largestword.Length)
+                {
+                    largestword = a[i];
+                }
+            }
+            Console.WriteLine(largestword);
+         
+        }
+    }
+}
