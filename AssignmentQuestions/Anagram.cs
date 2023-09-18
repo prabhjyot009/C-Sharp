b5812a8ae244baa796ac3d4c5e79ea35a083e424
prabhjyot009 <prabh156dps@gmail.com>
2023-09-19 01:09:46 +0530

    Assignment ques

---
+++ AssignmentQuestions/Anagram.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentQuestions
+{
+    class Anagram
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "CAT";
+            string str2 = "ACt";
+
+            char[] ch1 = str1.ToCharArray();
+            char[] ch2 = str2.ToCharArray();
+
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+
+            string val1 = new string(ch1);
+            string val2 = new string(ch2);
+
+            if (val1.ToUpper() == val2.ToUpper())
+            {
+                Console.WriteLine("is Anagram");
+            }
+            else
+            {
+                Console.WriteLine("is not anagram");
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- AssignmentQuestions/FirstLastCharacterUpper.cs
+++ AssignmentQuestions/FirstLastCharacterUpper.cs
@@ -7,7 +7,28 @@ using System.Threading.Tasks;
 namespace AssignmentQuestions
 {
     class FirstLastCharacterUpper
-    {
+    {        
+        static void Main(string[] args)
+        {
+            string str = "Take u forward is Awesome";
+            string[] words = str.Split(' ');
+            StringBuilder cap = new StringBuilder();
+            foreach (string w in words)
+            {
+                if (w.Length >= 2)
+                {
+                    string first = w.Substring(0, 1);
+                    string afterfirst = w.Substring(1, w.Length - 1);
+                    string last = w.Substring(w.Length - 1);
+                    cap.Append(first.ToUpper()).Append(afterfirst).Append(last.ToUpper()).Append(" ");
+                }
+                else
+                {
+                    cap.Append(w.ToUpper()).Append(" ");
+                }
+            }
+            Console.WriteLine(cap);
+        }
 
     }
 }
