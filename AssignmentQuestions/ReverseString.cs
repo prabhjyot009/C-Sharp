2dcf88de169ad82de999e1beff3bb2cac13601d5
prabhjyot009 <prabh156dps@gmail.com>
2023-09-18 23:36:22 +0530

    Assignment ques

---
+++ AssignmentQuestions/ReverseString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentQuestions
+{
+    class ReverseString
+    {
+        static void Main(string[] args)
+        {
+            string str = "Take u forward is Awesome";
+            string rev = "";
+            for (int i = str.Length-1; i >=0; i--)
+            {
+                rev += str[i].ToString();
+            }
+            Console.WriteLine(rev);
+        }
+    }
+}
---
+++ AssignmentQuestions/SumOfNumbersInString.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentQuestions
+{
+    class SumOfNumbersInString
+    {
+        static void Main(string[] args)
+        {
+            //string str = "1xyz23";
+            //char[] ch = str.ToCharArray();
+            //int sum = 0;
+            //for (int i = 0; i < ch.Length; i++)
+            //{
+            //    if (char.IsDigit(ch[i]))
+            //    {
+            //        sum += int.Parse(ch[i].ToString());
+            //    }
+            //}
+            //Console.WriteLine(sum);
+            //Console.ReadLine();
+            string str = "1xyz23";
+            string temp="";
+            int sum = 0;
+            foreach (char c in str)
+            {
+                if (char.IsDigit(c))
+                {
+                    temp += c;
+                }
+                else
+                {
+                    if (temp.Length > 0)
+                    {
+                        sum += int.Parse(temp);
+                        temp = "";
+                    }         
+                }
+            }
+            Console.WriteLine(sum+=int.Parse(temp));
+            Console.ReadLine();
+        }
+
+
+    }
+}
