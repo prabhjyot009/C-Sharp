f3ce01cde6ad002a8d3d336bab5a6d3d745a9a13
prabhjyot009 <prabh156dps@gmail.com>
2023-09-19 21:47:05 +0530

    commit

---
+++ Strivers-sheet/CountDigits.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strivers_sheet
+{
+    class CountDigits
+    {
+        static void Main(string[] args)
+        {
+            int num = 123;
+            int count = 0;
+            while (num!=0)
+            {
+                num = num / 10;
+                count++;
+            }
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Strivers-sheet/ReverseNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strivers_sheet
+{
+    class ReverseNumber
+    {        
+        static void Main(string[] args)
+        {
+            int num = 12345;
+            int rev = 0;
+            while (num!=0)
+            {
+                rev = (rev * 10) + num % 10;
+                num = num / 10;
+            }
+            Console.WriteLine(rev);
+            Console.ReadLine();
+        }
+    }
+}
