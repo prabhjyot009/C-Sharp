b333974e01971da2a66f8375eb377404c6aad45a
prabhjyot009 <prabh156dps@gmail.com>
2023-09-19 23:15:03 +0530

    Striver sheet

---
+++ Strivers-sheet/NaturalNumbersRecursion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strivers_sheet
+{
+    class NaturalNumbersRecursion
+    {
+        static void printnTimes(int n)
+        {
+            if (n == 1)
+            {
+                Console.Write("1");
+                return ;
+            }
+            printnTimes(n - 1);
+            Console.Write(" "+n+"");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter n times");
+            int n = Convert.ToInt32(Console.ReadLine());
+            printnTimes(n);
+        }
+    }
+}
---
+++ Strivers-sheet/PrintNameNtimes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strivers_sheet
+{
+    class PrintNameNtimes
+    {
+        static void printnTimes(int i,int n)
+        {
+            if (i > n)
+            {
+                return;
+            }
+            Console.WriteLine("Singh");
+            printnTimes(i + 1, n);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter n times");
+            int n=Convert.ToInt32(Console.ReadLine());
+            printnTimes(1,n);
+        }
+    }
+}
