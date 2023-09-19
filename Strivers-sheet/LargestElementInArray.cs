ba00d6c941e5f7d76c66a83c3b04643fbddb8639
prabhjyot009 <prabh156dps@gmail.com>
2023-09-20 01:13:36 +0530

    Striver sheet arrays

---
+++ Strivers-sheet/LargestElementInArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strivers_sheet
+{
+    class LargestElementInArray
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            int max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+            Console.WriteLine(max);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Strivers-sheet/SecondLargestInArray.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strivers_sheet
+{
+    class SecondLargestInArray
+    {
+        static void printArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]+" ");
+            }
+            Console.WriteLine();
+        }
+        static void secondLarge(int[] arr)
+        {
+            Array.Sort(arr);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length-i-1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine(arr[arr.Length-2]);
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = {4,5,7,8,2,1};
+            Console.WriteLine("Orignal Array");
+            printArray(arr);
+            Console.WriteLine("Sorted Array");
+            secondLarge(arr);
+        }
+    }
+}
