787564789637780be0dc5af3087baba5cd137b97
prabhjyot009 <prabh156dps@gmail.com>
2023-09-19 22:35:15 +0530

    strivers sheet

--- Strivers-sheet/AllDivisors.cs
+++ Strivers-sheet/AllDivisors.cs
@@ -8,5 +8,19 @@ namespace Strivers_sheet
 {
     class AllDivisors
     {
+        static void Main(string[] args)
+        {
+            int n = 36;
+            int i = 1;
+            while (i <= n)
+            {
+                if (n % i == 0)
+                {
+                    Console.Write(i+" ");
+                }
+                i++;
+            }
+            Console.ReadLine();
+        }
     }
 }
---
+++ Strivers-sheet/CheckPrime.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strivers_sheet
+{
+    class CheckPrime
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter prime:");
+            int n = Int32.Parse(Console.ReadLine());
+            bool isprime = true;
+            for (int i = 2; i <=n/2; i++)
+            {
+                if (n%i==0)
+                {
+                    isprime = false;
+                    break;
+                }
+            }
+            if (n!=1 && isprime)
+            {
+                Console.WriteLine("it is prime");
+            }
+            else
+            {
+                Console.WriteLine("it is not prime");
+            }
+
+        }
+    }
+}
