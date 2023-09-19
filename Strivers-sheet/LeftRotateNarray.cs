33483743d46dfb2104e04ad4e043142ea88a5a94
prabhjyot009 <prabh156dps@gmail.com>
2023-09-20 02:35:20 +0530

    striver sheet

---
+++ Strivers-sheet/LeftRotateNarray.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strivers_sheet
+{
+    class LeftRotateNarray
+    {
+        static void printArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        static void leftRotateN(int[] arr,int k)
+        {
+            int n = arr.Length;
+            k = k % n;
+            reversePart(arr, 0, n - 1);
+            reversePart(arr, 0, k - 1);
+            reversePart(arr, k , n-1);
+            printArray(arr);
+        }
+        static void reversePart(int[] arr,int i,int j)
+        {
+            while (i < j)
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            int k = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Orignal Array");
+            printArray(arr);
+            Console.WriteLine("New Array:");
+            leftRotateN(arr,k);
+        }
+    }
+}
---
+++ Strivers-sheet/MoveZeroesToEnd.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strivers_sheet
+{
+    class MoveZeroesToEnd
+    {
+        static void printArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        static void moveZeros(int[] arr)
+        {
+            int left = 0;
+            int right = 0;
+            int n = arr.Length;
+            while (right < n)
+            {
+                if (arr[right] != 0)
+                {
+                    swap(arr, left, right);
+                    left++;
+                }
+                right++;
+            }
+            printArray(arr);
+        }
+        static void swap(int[] arr, int i,int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 0, 0, 4, 5 };
+            Console.WriteLine("Orignal Array");
+            printArray(arr);
+            Console.WriteLine("New Array:");
+            moveZeros(arr);
+        }
+    }
+}
