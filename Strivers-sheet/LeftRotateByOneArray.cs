0ebaa422b85ae4fd08a70db48082f13535c7c11b
prabhjyot009 <prabh156dps@gmail.com>
2023-09-20 02:10:30 +0530

    striver sheet

---
+++ Strivers-sheet/LeftRotateByOneArray.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strivers_sheet
+{
+    class LeftRotateByOneArray
+    {
+        static void printArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        static void leftRotateOne(int[] arr)
+        {
+            int temp = arr[0];
+            int n = arr.Length;
+            for (int i = 0; i < n-1; i++)
+            {
+                arr[i] = arr[i + 1];
+            }
+            arr[n - 1] = temp;
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(arr[i]+" ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Orignal Array");
+            printArray(arr);
+            Console.WriteLine("New Array:");
+            leftRotateOne(arr);
+        }
+    }
+}
---
+++ Strivers-sheet/RemoveDuplicatesFromArray.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strivers_sheet
+{
+    class RemoveDuplicatesFromArray
+    {
+        static void printArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        static void removeDuplicates(int[] arr)
+        {
+            for (int i = 0; i <arr.Length; i++)
+            {
+                int idx = Math.Abs(arr[i]);
+                if (arr[idx] > 0)
+                {
+                    Console.Write(idx +" ");
+                }
+                arr[idx] =- arr[idx];
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 1, 2, 3, 4, 5 ,5};
+            Console.WriteLine("Orignal Array");
+            printArray(arr);
+            Console.WriteLine("New Array:");
+            removeDuplicates(arr);
+        }
+    }
+}
