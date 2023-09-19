ca9d560588cef9711d47cd0ef49b9f7c94c1964c
prabhjyot009 <prabh156dps@gmail.com>
2023-09-19 22:11:10 +0530

    Striver Sheet

---
+++ Strivers-sheet/Armstrong.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strivers_sheet
+{
+    class Armstrong
+    {
+        static void Main(string[] args)
+        {
+            int num = 153;
+            int sum = 0;
+            int temp = num;
+            while (num != 0)
+            {
+                sum += (num % 10) * (num % 10) * (num % 10);
+                num = num / 10;
+            }
+            if (temp == sum)
+            {
+                Console.WriteLine("It is armstrong"); 
+            }
+            else
+            {
+                Console.WriteLine("Not armstrong");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Strivers-sheet/GCDorHCF.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strivers_sheet
+{
+    class GCDorHCF
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;  
+            int num1 = 12;
+            int num2 = 15;
+            int gcd = 1;
+            while (i<=num1 && i<=num2)
+            {
+                if(num1%i==0 && num2 % i == 0)
+                {
+                    gcd = i;
+                }
+                i++;
+            }
+            Console.WriteLine(gcd);
+            Console.ReadLine();
+        }
+    }
+}
