aa09dce30b824570227b0ffdf4dc47ea5b4aa0d0
prabhjyot009 <prabh156dps@gmail.com>
2023-09-21 22:19:31 +0530

    sorting techniques

---
+++ Strivers-sheet/InsertionSort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strivers_sheet
+{
+    class InsertionSort
+    {
+        static void printArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]+" ");
+            }
+            Console.WriteLine();
+        }
+        static void insertSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 1; i < n; i++)
+            {
+                int j = i;
+                while (j>0 && arr[j] < arr[j-1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    j--;
+                }
+            }
+            printArray(arr);
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 4, 3, 2, 1 };
+            Console.WriteLine("Orignal Array:");
+            printArray(arr);
+            Console.WriteLine("Sorted Array:");
+            insertSort(arr);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Strivers-sheet/SelectionSort.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strivers_sheet
+{
+    class SelectionSort
+    {
+        static void printArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        static void selectSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; i++)
+            {
+                int minIndex = i;
+                for (global::System.Int32 j = i+1; j < n; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                if (minIndex != i)
+                {
+                    int temp = arr[minIndex];
+                    arr[minIndex] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+            printArray(arr);
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 4, 3, 2, 1 };
+            Console.WriteLine("Orignal Array:");
+            printArray(arr);
+            Console.WriteLine("Sorted Array:");
+            selectSort(arr);
+            Console.ReadLine();
+        }
+    }
+}
