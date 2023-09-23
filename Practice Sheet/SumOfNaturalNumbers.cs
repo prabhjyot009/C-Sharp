3e5c1384bf1043faee73149640f31e2036c03eb1
prabhjyot009 <prabh156dps@gmail.com>
2023-09-23 16:36:46 +0530

    commit

---
+++ Practice Sheet/SumOfNaturalNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Sheet
+{
+    class SumOfNaturalNumbers
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            int n = Int32.Parse(Console.ReadLine());
+            int i = 1;
+            int sum = 0;
+
+            while (i <= n)
+            {
+                sum = sum + i;                
+                i++;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
