e74cce004216de62adf2e962e35303f252db344b
prabhjyot009 <prabh156dps@gmail.com>
2023-09-23 17:03:45 +0530

    practice sheet

---
+++ Practice Sheet/CheckPalindrome.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Sheet
+{
+    class CheckPalindrome
+    {
+        static void Main(string[] args)
+        {
+            int n = 1221;
+            int rev = 0;
+            int temp = n;
+            while (n > 0)
+            {
+                rev = (rev * 10) + n % 10;
+                n = n / 10;
+            }
+            if (temp == rev)
+            {
+                Console.WriteLine("it is palindrome");
+            }
+            else
+            {
+                Console.WriteLine("it is not palindrome");
+            }
+        }
+    }
+}
