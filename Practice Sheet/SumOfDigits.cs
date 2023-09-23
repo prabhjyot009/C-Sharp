a36d3192ce0c84f43d4b335eab73659533d0c500
prabhjyot009 <prabh156dps@gmail.com>
2023-09-23 16:31:38 +0530

    practice session

---
+++ Practice Sheet/SumOfDigits.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Sheet
+{
+    class SumOfDigits
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int n = 123;
+            int sum = 0;
+            while (n > 0)
+            {
+                sum = sum + (n % 10);
+                n = n / 10;
+                i++;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Practice Sheet/TempratureConvertor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Sheet
+{
+    class TempratureConvertor
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ENter celsius");
+            double celsius = double.Parse(Console.ReadLine());
+            Console.WriteLine("Celsius to fahrenheit is: "); celsiusTofahren(celsius);
+            Console.WriteLine("Enter Fahrenheit");
+            double fahren = double.Parse(Console.ReadLine());
+            Console.WriteLine("Fahrenheit to Celsius is: "); fahrenTocelsius(fahren);
+        }
+        static void celsiusTofahren(double celsius)
+        {
+            double fahrenheit = ((celsius * 9) / 5) + 32;
+            Console.WriteLine(fahrenheit);
+        }
+        static void fahrenTocelsius(double fahren)
+        {
+            double celsius = ((fahren - 32) * 5) /9;
+            Console.WriteLine(celsius);
+        }
+    }
+}
