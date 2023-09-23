f9a0b7fdb0ff1de8620c9b21015778883c766945
prabhjyot009 <prabh156dps@gmail.com>
2023-09-23 14:01:32 +0530

    practice session commits

---
+++ Strivers-sheet/NPrime.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strivers_sheet
+{
+    class NPrime
+    {
+        static bool isPrime(int n)
+        {
+            if(n==1 || n == 0)
+            {
+                return false;
+            }
+            for (int i = 2; i < n; i++)
+            {
+                if (n % 2 == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter n: ");
+            int n = Int32.Parse(Console.ReadLine());
+            for(int i = 1; i <= n; i++)
+            {
+                if (isPrime(i))
+                {
+                    Console.Write(i+" ");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
