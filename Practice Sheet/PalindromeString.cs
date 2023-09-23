4c14b60fd49f8dbb07090eba9aa46dd71ae47ea3
prabhjyot009 <prabh156dps@gmail.com>
2023-09-23 22:04:40 +0530

    practice sheet

---
+++ Practice Sheet/PalindromeString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Sheet
+{
+    class PalindromeString
+    {
+        static void Main(string[] args)
+        {
+            string str = "heleh";
+            string rev = "";
+            for (int i = str.Length-1; i >=0; i--)
+            {
+                rev += str[i];
+            }
+            if (rev == str)
+            {
+                Console.WriteLine("It is palindrome");
+            }
+            else
+            {
+                Console.WriteLine("it is not palindrome");
+            }
+        }
+    }
+}
---
+++ Practice Sheet/PatternOne.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Sheet
+{
+    class PatternOne
+    {
+        static void Main(string[] args)
+        {
+            //int NoOfRows = 5;
+            //for (int rows = 1; rows <= NoOfRows; rows++)
+            //{
+            //    for (int cols = 1; cols <= rows; cols++)
+            //    {
+            //        Console.Write(rows+" ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            int NoOfRows = 6;
+            for (int i = 1; i < NoOfRows; i++)
+            {
+                for (global::System.Int32 j = 1; j <= i; j++)
+                {
+                    Console.Write(j+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
