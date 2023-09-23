b91e859e487f30d80bfddf867233e61305d8419d
prabhjyot009 <prabh156dps@gmail.com>
2023-09-23 14:52:32 +0530

    commit

---
+++ Strivers-sheet/RangePrime.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strivers_sheet
+{
+    class RangePrime
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 10;
+            int n2 = 18;
+            int flag;
+
+            for (int i = n1; i <=n2; i++)
+            {
+                if(i == 1 || i == 0)
+                {
+                    continue;
+                }
+                flag = 1;
+                for (int j = 2; j < i/2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        flag = 0;
+                        break;
+                    }
+                }                
+                if(flag == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
