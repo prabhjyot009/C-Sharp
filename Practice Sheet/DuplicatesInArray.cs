918bb31fc7dc44209fce1f46abea6168c1d09725
prabhjyot009 <prabh156dps@gmail.com>
2023-09-24 21:57:58 +0530

    commit

---
+++ Practice Sheet/DuplicatesInArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Sheet
+{
+    class DuplicatesInArray
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 1, 3, 3 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int idx = Math.Abs(arr[i]);
+                if (arr[idx] < 0)
+                {
+                    Console.Write(idx + " ");
+                }
+                else
+                {
+                    arr[idx] = -arr[idx];
+                }
+            }
+        }
+    }
+}
---
+++ Practice Sheet/SortedOrNot.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Sheet
+{
+    class SortedOrNot
+    {
+        static void printArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]+" ");
+            }
+            Console.WriteLine();
+        }
+        static void sorted(int[] arr)
+        {
+            int n = arr.Length;
+            bool check = true;
+            for (int i = 0; i < n-1; i++)
+            {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        check = false;
+                    }
+            }
+            if (check)
+            {
+                Console.WriteLine("sorted");
+            }
+            else
+            {
+                Console.WriteLine("Not sorted");
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { -1, 0 };
+            Console.WriteLine("Orignal Array:");
+            printArray(arr);
+            sorted(arr);
+        }
+    }
+}
