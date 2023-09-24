a3b5bcace7c31db06b38e5852e1e89099081c345
prabhjyot009 <prabh156dps@gmail.com>
2023-09-24 14:20:26 +0530

    commit

---
+++ Practice Sheet/PatternSeven.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Practice_Sheet
+{
+    class PatternSeven
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rows:");
+            int noOfrows = Int32.Parse(Console.ReadLine());
+            for (int rows = noOfrows; rows>=1; rows--)
+            {
+                for(int cols = noOfrows; cols >=rows; cols--)
+                {
+                    Console.Write(cols);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
