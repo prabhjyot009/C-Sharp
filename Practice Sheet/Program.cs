9fcd0dda1ce508b372adf6a7b6cf12f8da7139b2
prabhjyot009 <prabh156dps@gmail.com>
2023-09-26 00:53:10 +0530

    commit

--- Practice Sheet/Program.cs
+++ Practice Sheet/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 
 internal class Program
 {
     static void Main(string[] args)
     {
         //palindrome string:
+
         /*Console.Write("Enter string: ");
         string str = Convert.ToString(Console.ReadLine());
         string temp = str;
@@ -21,29 +24,127 @@ internal class Program
         {
             Console.WriteLine("It is not palindrome");
         }*/
+
         //Count vovels consonants and white spaces:
+
+        //Console.Write("Enter string: ");
+        //string str = Convert.ToString(Console.ReadLine());
+        //int vovels = 0, consonants = 0, space = 0;
+        //for(int i=0;i<str.Length;i++)
+        //{
+        //    char ch = str[i];
+        //    ch = char.ToLower(str[i]);
+        //    if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch == 'u')
+        //    {
+        //        vovels++;
+        //    }
+        //    else if(ch==' ')
+        //    {
+        //        space++;
+        //    }
+        //    else
+        //    {
+        //        consonants++;
+        //    }            
+        //}
+        //Console.WriteLine(vovels);
+        //Console.WriteLine(space);
+        //Console.WriteLine(consonants);
+
+        //remove vovels:
+
+        //Console.Write("Enter string: ");
+        //string str = Convert.ToString(Console.ReadLine());
+        //string str1 = "";
+        //for(int i=0;i<str.Length;i++)
+        //{
+        //    char ch= str[i];
+        //    ch=char.ToLower(str[i]);
+        //    if(ch!='a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u')
+        //    {
+        //        str1 += ch.ToString();
+        //    }
+        //}
+        //Console.WriteLine(str1);
+        //Console.ReadLine();
+
+        //remove white spaces:
+
+        //Console.Write("Enter string: ");
+        //string str = Convert.ToString(Console.ReadLine());
+        //string str1 = "";
+        //for (int i = 0; i < str.Length; i++)
+        //{
+        //    char ch = str[i];
+        //    if (ch != ' ')
+        //    {
+        //        str1 += ch.ToString();
+        //    }
+        //}
+        //Console.WriteLine(str1);
+        //Console.ReadLine();
+
+        //remove expressions:
+
+        //Console.Write("Enter string: ");
+        //string str = Convert.ToString(Console.ReadLine());
+        //string str1 = "";
+        //for (int i = 0; i < str.Length; i++)
+        //{
+        //    char ch = str[i];
+        //    if (char.IsLetter(str[i]))
+        //    {
+        //        str1 += ch.ToString();
+        //    }
+        //}
+        //Console.WriteLine(str1);
+        //Console.ReadLine();
+
+        //add 1xyz23;
+        
+        //Console.Write("Enter string: ");
+        //string str = Convert.ToString(Console.ReadLine());
+        //string temp = "";
+        //int sum = 0;
+        //for (int i = 0; i < str.Length; i++)
+        //{
+        //    char ch = str[i];
+        //    if (char.IsDigit(str[i])){
+        //        temp += ch;
+        //    }
+        //    else
+        //    {
+        //        sum += int.Parse(temp);
+        //        temp = "0";
+        //    }
+        //}
+        //Console.WriteLine(sum+int.Parse(temp));
+
+        //capitalize first and last:
+
+        //Console.Write("Enter string: ");
+        //string str = Convert.ToString(Console.ReadLine());
+        //string[] word = str.Split();
+        //StringBuilder cap = new StringBuilder();
+        //foreach (string w in word)
+        //{
+        //    if (w.Length > 2)
+        //    {
+        //        string first = w.Substring(0, 1);
+        //        string afterfirst = w.Substring(1, w.Length - 2);
+        //        string last = w.Substring(w.Length - 1);
+        //        cap.Append(first.ToUpper()).Append(afterfirst).Append(last.ToUpper()).Append(" ");
+        //    }
+        //    else
+        //    {
+        //        cap.Append(w.ToUpper()).Append(" ");
+        //    }
+        //}
+        //Console.WriteLine(cap);
+
+
         Console.Write("Enter string: ");
-        string str = Convert.ToString(Console.ReadLine());
-        int vovels = 0, consonants = 0, space = 0;
-        foreach (int i in str)
-        {
-            char ch = str[i];
-            ch.ToString().ToLower();
-            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch == 'u')
-            {
-                vovels++;
-            }
-            if(ch==' ')
-            {
-                space++;
-            }
-            else
-            {
-                consonants++;
-            }
-        }
-        Console.WriteLine(vovels);
-        Console.WriteLine(space);
-        Console.WriteLine(consonants);
+        string str1 = Convert.ToString(Console.ReadLine());
+        Console.WriteLine("");
     }
 }
