d98e167182a9b0217aaae5650f4905b19c04d3c9
prabhjyot009 <prabh156dps@gmail.com>
2023-09-25 23:39:33 +0530

    commit

--- Practice Sheet/Program.cs
+++ Practice Sheet/Program.cs
@@ -2,33 +2,48 @@
 
 internal class Program
 {
-    private static void Main(string[] args)
+    static void Main(string[] args)
     {
-        int[] a = { 1, 2, 0, 1, 0, 1 };
-        int j = 0;
-        for (int i = 0; i < a.Length; i++)
+        //palindrome string:
+        /*Console.Write("Enter string: ");
+        string str = Convert.ToString(Console.ReadLine());
+        string temp = str;
+        string rev = "";
+        for (int i = str.Length - 1; i >= 0; i--)
         {
-            if (a[i] != 0)
+            rev += str[i];
+        }
+        if (temp == rev)
+        {
+            Console.WriteLine("it is palindrome");
+        }
+        else
+        {
+            Console.WriteLine("It is not palindrome");
+        }*/
+        //Count vovels consonants and white spaces:
+        Console.Write("Enter string: ");
+        string str = Convert.ToString(Console.ReadLine());
+        int vovels = 0, consonants = 0, space = 0;
+        foreach (int i in str)
+        {
+            char ch = str[i];
+            ch.ToString().ToLower();
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch == 'u')
             {
-                a[j] = a[i];
-                j++;
+                vovels++;
+            }
+            if(ch==' ')
+            {
+                space++;
+            }
+            else
+            {
+                consonants++;
             }
         }
-
-        foreach (int ele in a)
-        {
-            Console.Write(ele + " ");
-        }
-
-        while (j < a.Length)
-            a[j++] = 0;
-
-        foreach (int ele in a)
-        {
-            Console.Write(ele + " ");
-        }
-
-        Console.WriteLine("hhe");
-        Console.ReadLine();
+        Console.WriteLine(vovels);
+        Console.WriteLine(space);
+        Console.WriteLine(consonants);
     }
 }
