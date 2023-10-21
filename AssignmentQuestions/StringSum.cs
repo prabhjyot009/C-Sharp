b546c857d85336271eb9a8cd143118b174d16c27
prabhjyot009 <prabh156dps@gmail.com>
2023-10-21 21:34:22 +0530

    String sum

---
+++ AssignmentQuestions/StringSum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+namespace AssignmentQuestions
+{
+    class StringSum
+    {
+        static void Main()
+        {
+            string input = "1xy23";
+            int sum = 0;
+            foreach (char character in input)
+            {
+                if (char.IsDigit(character))
+                {
+                    sum += int.Parse(character.ToString());
+                }
+            }
+            Console.WriteLine("Sum of digits in the string: " + sum);
+        }
+    }
+
+}
