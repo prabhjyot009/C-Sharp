bc2da4158ea8a8a3e64a6d2bfceae1d17bf8f8b3
prabhjyot009 <prabh156dps@gmail.com>
2023-10-21 21:44:58 +0530

    second largest

---
+++ AssignmentQuestions/SecondLargestNumber.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentQuestions
+{
+    class SecondLargestNumber
+    {
+        static void printArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]+" ");
+            }
+            Console.WriteLine();
+        }
+        static void secondLarge(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for(int j = i; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Second Largest Number is: " + arr[arr.Length-2]);
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 2,3,4,5,1,9,8};
+            printArray(arr);
+            secondLarge(arr);
+            Console.ReadLine();
+        }   
+    }
+}
