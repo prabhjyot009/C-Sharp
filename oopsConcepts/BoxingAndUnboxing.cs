89678367782e5ec7119614e797df9e2a62a83243
prabhjyot009 <prabh156dps@gmail.com>
2023-10-22 20:28:51 +0530

    commit

---
+++ oopsConcepts/BoxingAndUnboxing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopsConcepts
+{
+    class BoxingAndUnboxing
+    {
+        static void Main(string[] args)
+        {
+            int i = 123;
+            object o = i; // Boxing
+            int j = (int)o; // Unboxing
+            Console.WriteLine(j);
+            Console.ReadLine();
+        }
+    }
+}
