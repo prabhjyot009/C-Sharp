7fc990325c56e8f6ff4d496cbea2f9e5904878c9
prabhjyot009 <prabh156dps@gmail.com>
2023-10-23 00:17:49 +0530

    Constructors

---
+++ oopsConcepts/ConstructorProg.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopsConcepts
+{
+    class ConstructorProg
+    {
+        //if we are not creating any constructor in our program then
+        //csharp complier will automatically create an constructor
+        //also known as default constructor.
+        public ConstructorProg()
+        {
+            Console.WriteLine("Constructor invoked");
+        }
+        static void Main(string[] args)
+        {
+            ConstructorProg obj = new ConstructorProg();
+        }
+    }
+}
---
+++ oopsConcepts/ParametrizedConstructor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopsConcepts
+{
+    class ParametrizedConstructor
+    {
+            int EmpId;
+            string EmpName;
+            int EmpAge;
+            public ParametrizedConstructor(int EmpId, string EmpName, int EmpAge)
+            {
+                this.EmpId = EmpId;
+                this.EmpName = EmpName;
+                this.EmpAge = EmpAge;
+            }
+            public int getId()
+            {
+                return this.EmpId;
+            }
+            public string getName()
+            {
+                return this.EmpName;
+            }
+            public int getAge()
+            {
+                return this.EmpAge;
+            }
+            static void Main(string[] args)
+            {
+                ParametrizedConstructor singh = new ParametrizedConstructor(11, "Singh", 23);
+                ParametrizedConstructor brar = new ParametrizedConstructor(12, "Brar", 20);
+                Console.WriteLine("Employee Id is {0}", singh.getId());
+                Console.WriteLine("Employee Name is {0}", singh.getName());
+                Console.WriteLine("Employee Age is {0}", singh.getAge());
+                Console.WriteLine("------------------------");
+                Console.WriteLine("Employee Id is {0}", brar.getId());
+                Console.WriteLine("Employee Name is {0}", brar.getName());
+                Console.WriteLine("Employee Age is {0}", brar.getAge());
+                Console.ReadLine();
+            }
+        }
+    }
