a897487d6bca0f7d931bf9ea3c62d850055f0a04
prabhjyot009 <prabh156dps@gmail.com>
2023-11-14 15:11:50 +0530

    constructors

--- oopsConcepts/Copy Constructor.cs
+++ oopsConcepts/Copy Constructor.cs
@@ -6,8 +6,39 @@ using System.Threading.Tasks;
 
 namespace oopsConcepts
 {
+    class example
+    {
+        /*The constructor which creates an object by copying variables from another object is called
+         * a copy constructor. The purpose of a copy constructor is to initialize
+         * a new instance to the values of an existing instance.
+         */
+        string name;
+        int age;
+        public example(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public example(example e)
+        {
+            this.name = e.name;
+            this.age = e.age;
+        }
+        public void getData()
+        {
+            Console.WriteLine("Name is: {0}", name);
+            Console.WriteLine("Age is: {0}", age);
+        }
+    }
     class Copy_Constructor
     {
-
+        static void Main(string[] args)
+        {
+            example obj = new example("Singh", 20);
+            obj.getData();
+            example obj1 = new example(obj);
+            obj1.getData();
+            Console.ReadLine();
+        }
     }
 }
